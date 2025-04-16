42b19e360eee36cbe4a07e3355718ac968cf1825
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2025-04-15 21:37:02 -0300

    inicializando o projeto v2

---
+++ backend-v2/src/test/java/br/com/gabrielferreira/contratos/ContratosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielferreira.contratos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
